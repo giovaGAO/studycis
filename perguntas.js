8e46293b838702d3794ed147d4256feaa9b26cdd
Giovanna Andrade <giovannamaio2006@gmail.com>
2024-11-27 17:43:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astrologia',
+    'O que é astrologia?',
+    'Estudo da relação entre os astros e a vida humana.'
+)
+
+criaCartao(
+    'Astrologia',
+    'Quais são os principais componentes da astrologia?',
+    'Signos zodiacais, planetas, casas astrológicas e aspectos.'
+)
+
+criaCartao(
+    'Astrologia',
+    'Quantos signos zodiacais existem?',
+    '12 (Áries, Touro, Gêmeos, Câncer, Leão, Virgem, Libra, Escorpião, Sagitário, Capricórnio, Aquário e Peixes).'
+)
+
+criaCartao(
+    'Astrologia',
+    'O que é astrologia natal?',
+    ' Estudo do horóscopo de nascimento para entender personalidade e destino.'
+)
